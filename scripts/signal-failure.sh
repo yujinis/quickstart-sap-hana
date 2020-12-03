99e9b2663df232a5dad16abbefd0cf40deb9f64a
Guilherme Felix <fguilher@amazon.com>
2020-12-03 10:06:17 +0000

    Added SAP HANA version check logic

--- scripts/signal-failure.sh
+++ scripts/signal-failure.sh
@@ -42,76 +42,72 @@ log `date` signal-failure
 
 case "$1" in
 
-	HANAINSTALLFAIL) log "The HANA installation did not succeed. Please check installation media."
+	HANAINSTALLFAIL) 
+	    log "The HANA installation did not succeed. Please check installation media."
+	    curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA installation did not succeed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA installation did not succeed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    ;;
+	    
+    HANACHECKFAIL)
+        log "The HANA version check did not suceed. Please check installation media."
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA version check did not suceed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA version check did not suceed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    ;;
+	
+	INCOMPATIBLE)
+	    log "Instance Type = X1 and O.S. is not supported with X1"
+	    curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "X1 instance type requires minimum kernel version of 3.10, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "X1 instance type requires minimum kernel version of 3.10, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+	    ;;
 
-	curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA installation did not succeed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+      INCOMPATIBLE_RHEL) 
+        log "Instance Type = X1 and RHEL 6.6 is not supported with X1"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "RHEL 6.6 is not compatible with X1 instance type. HANA on X1 requires minimum version of RHEL6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "RHEL 6.6 is not compatible with X1 instance type. HANA on X1 requires minimum version of RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "The HANA installation did not succeed. Please check installation media.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-	;;
+      INCOMPATIBLE_RHEL_SCALEOUT)
+        log "RHEL 6.7 is not supported with X1 Scaleout"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Scale out deployment with X1 instance type is not compatible with RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Scale out deployment with X1 instance type is not compatible with RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      INCOMPATIBLE) log "Instance Type = X1 and O.S. is not supported with X1"
+      YUM)
+        log "Instance Type = X1 and yum repo is not supported"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access RedHat update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access RedHat update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "X1 instance type requires minimum kernel version of 3.10, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+      ZYPPER)
+        log "Instance Type = X1 and zypper repo is not supported"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access SUSE update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access SUSE update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "X1 instance type requires minimum kernel version of 3.10, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
+      SUSECONNECTFAIL)
+        log "`date` SUSE BYOS registration did not succeed"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE BYOS registration did not succeed. Check SUSE registration code or internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE BYOS registration did not succeed. Check SUSE registration code or internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      INCOMPATIBLE_RHEL) log "Instance Type = X1 and RHEL 6.6 is not supported with X1"
+      ACTMODULEFAIL)
+        log "`date` SUSE public cloud module activation FAILED"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE public cloud module activation FAILED. Check internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE public cloud module activation FAILED. Check internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "RHEL 6.6 is not compatible with X1 instance type. HANA on X1 requires minimum version of RHEL6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+      EMPTY_STORAGE_JSON) 
+        log "`date` Valid storage.json file not found"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "storage.json file not found or found empty . If custom storage.json is used, check file permission.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "storage.json file not found or found empty. If custom storage.json is used, check file permission.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        ;;
 
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "RHEL 6.6 is not compatible with X1 instance type. HANA on X1 requires minimum version of RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      INCOMPATIBLE_RHEL_SCALEOUT) log "RHEL 6.7 is not supported with X1 Scaleout"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Scale out deployment with X1 instance type is not compatible with RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Scale out deployment with X1 instance type is not compatible with RHEL 6.7, Choose the right operating System and try again","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      YUM) log "Instance Type = X1 and yum repo is not supported"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access RedHat update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access RedHat update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      ZYPPER) log "Instance Type = X1 and zypper repo is not supported"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access SUSE update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "Not able to access SUSE update repository, package installation may fail","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      SUSECONNECTFAIL) log "`date` SUSE BYOS registration did not succeed"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE BYOS registration did not succeed. Check SUSE registration code or internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE BYOS registration did not succeed. Check SUSE registration code or internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      ACTMODULEFAIL) log "`date` SUSE public cloud module activation FAILED"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE public cloud module activation FAILED. Check internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "SUSE public cloud module activation FAILED. Check internet connection","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      EMPTY_STORAGE_JSON) log "`date` Valid storage.json file not found"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "storage.json file not found or found empty . If custom storage.json is used, check file permission.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "storage.json file not found or found empty. If custom storage.json is used, check file permission.","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      ;;
-
-      *) log "Function Not Implemented"
-
-      curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "signal-failure function not implemented","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-
-      echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "signal-failure function not implemented","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
-      exit 1
-      esac
+      *)
+        log "Function Not Implemented"
+        curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "signal-failure function not implemented","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        echo curl -X PUT -H 'Content-Type:' --data-binary '{"Status" : "FAILURE","Reason" : "signal-failure function not implemented","UniqueId" : "HANAMaster","Data" : "Failure"}' "${WaitForMasterInstallWaitHandle}"
+        exit 1
+esac
 
 #fi
 
